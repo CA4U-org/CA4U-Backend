97f06a3da999b38ec55e4c32cbd3885da68cc81c
seobeomseok <sbslc2000@gmail.com>
2024-11-03 14:42:18 +0900

    feat: rename HashtagResponseDto (typo error)

--- src/main/java/com/example/ca4u_backend/domain/hashtag/HashtagService.java
+++ src/main/java/com/example/ca4u_backend/domain/hashtag/HashtagService.java
@@ -1,6 +1,6 @@
 package com.example.ca4u_backend.domain.hashtag;
 
-import com.example.ca4u_backend.domain.hashtag.dto.HashtagReponseDto;
+import com.example.ca4u_backend.domain.hashtag.dto.HashtagResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,8 +13,8 @@ import java.util.List;
 public class HashtagService {
     private final HashtagRepository hashtagRepository;
 
-    public List<HashtagReponseDto> getClubAllHashtags(long clubId) {
+    public List<HashtagResponseDto> getClubAllHashtags(long clubId) {
         List<Hashtag> hashtags = hashtagRepository.findAllByClubId(clubId);
-        return hashtags.stream().map(HashtagReponseDto::of).toList();
+        return hashtags.stream().map(HashtagResponseDto::of).toList();
     }
 }
