002403c18aa51fdbfc707ca8bfea2f4dab233ab1
seobeomseok <sbslc2000@gmail.com>
2024-11-06 00:14:55 +0900

    feat: add debugging log for anonymous

--- src/main/java/com/example/ca4u_backend/common/auth/AuthenticationInfoMethodArgumentResolver.java
+++ src/main/java/com/example/ca4u_backend/common/auth/AuthenticationInfoMethodArgumentResolver.java
@@ -1,6 +1,7 @@
 package com.example.ca4u_backend.common.auth;
 
 import com.example.ca4u_backend.domain.user.dto.CustomOAuth2User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +15,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
  * Handler Parameter에 로그인한 사용자의 ID를 주입합니다.
  */
 @Component
+@Slf4j
 public class AuthenticationInfoMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
@@ -26,6 +28,8 @@ public class AuthenticationInfoMethodArgumentResolver implements HandlerMethodAr
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication.isAuthenticated() && !isAnonymous(authentication)) {
+
+
             CustomOAuth2User principal = (CustomOAuth2User) authentication.getPrincipal();
             return principal.getId();
         } else {
@@ -34,6 +38,8 @@ public class AuthenticationInfoMethodArgumentResolver implements HandlerMethodAr
     }
 
     private boolean isAnonymous(Authentication authentication) {
-        return authentication == null || "anonymousUser".equals(authentication.getPrincipal());
+        boolean ret =  authentication == null || "anonymousUser".equals(authentication.getPrincipal());
+        log.debug("isAnonymous: {}", ret);
+        return ret;
     }
 }
