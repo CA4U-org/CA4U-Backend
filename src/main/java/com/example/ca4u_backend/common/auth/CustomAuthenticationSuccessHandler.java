09f2bc60d7747aa63773d4326bba2569c6c7dfec
seobeomseok <sbslc2000@gmail.com>
2024-11-06 13:55:22 +0900

    feat: add some debugging log

--- src/main/java/com/example/ca4u_backend/common/auth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/ca4u_backend/common/auth/CustomAuthenticationSuccessHandler.java
@@ -3,18 +3,21 @@ package com.example.ca4u_backend.common.auth;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         // 요청의 호스트를 기준으로 리다이렉트 URL 결정
         String host = request.getHeader("Host");
+        log.debug("Host: {}", host);
         String redirectUri;
 
         if (host.contains("localhost")) {
