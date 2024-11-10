a3b100437b27c65a9b120ad55fb68c089f432fe4
seobeomseok <sbslc2000@gmail.com>
2024-11-10 14:45:13 +0900

    refactor: ai recommndation feature now encapsulate in clubService

--- src/main/java/com/example/ca4u_backend/domain/club/ClubService.java
+++ src/main/java/com/example/ca4u_backend/domain/club/ClubService.java
@@ -1,5 +1,6 @@
 package com.example.ca4u_backend.domain.club;
 
+import com.example.ca4u_backend.aiClient.AIClient;
 import com.example.ca4u_backend.domain.club.dto.ClubReponseDto;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class ClubService {
     private final ClubRepository clubRepository;
+    private final AIClient aiClient;
 
     public ClubReponseDto getClubSpec(long clubId) {
         Club club = clubRepository.findById(clubId).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 클럽입니다."));
@@ -30,8 +33,9 @@ public class ClubService {
         return clubList.stream().map(ClubReponseDto::of).toList();
     }
 
-    public List<ClubReponseDto> getClubsInIds(Collection<Long> clubIds) {
-        List<Club> clubList = clubRepository.findByIdIn(clubIds);
-        return clubList.stream().map(ClubReponseDto::of).toList();
+    public List<ClubReponseDto> getRelatedClubs(Long clubId) {
+        Set<Long> recommendedClubIds = aiClient.recommendedByContent(clubId);
+        List<Club> clubs = clubRepository.findByIdIn(recommendedClubIds);
+        return clubs.stream().map(ClubReponseDto::of).toList();
     }
 }
